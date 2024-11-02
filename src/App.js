511b944b0defb51d5e156413294e22b6296d506f
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-11-02 17:34:55 +0530

    using useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  //let count = 0;
+
+  const [count, serCount] = useState(0);
+
+  const handleIncrement = () => {
+    serCount(count + 1);
+    //count ++;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {count}
+      <br />
+      <button onClick={handleIncrement}>+ Increment</button>
+    </>
   );
 }
 
